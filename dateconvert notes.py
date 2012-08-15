d3ce37fbfa8f78105d74436756aece1b24d38218
sean <sean>
2012-08-15 19:40:05 -0400

    added formatDate() converted tabs to spaces Compiles

---
+++ dateconvert notes.py
@@ -0,0 +1,23 @@
+import datetime 
+
+a = date.today()
+a.today()
+a 
+
+%a W
+%A WW
+%b MMM
+%B MMMM
+%d DD, D
+%I HH
+%m M, MM
+%M 
+%p
+%S
+%U
+%w
+%W
+%x default
+%X HH:MM
+%y
+%Y
--- docengine.py
+++ docengine.py
@@ -2,77 +2,110 @@ import re
 import pymssql
 
 askvalue = {'docid':7,
-			'ORDER_NUMBER':'10250' }
+            'ORDER_NUMBER':'10250' }
 
 
 def sqlData(sql, dict = False):
-	'''returns data from any sql query'''
-	conn = pymssql.connect(host='server\sql2008', user='python', password='sql', database='northwind', as_dict='True')
-	cur = conn.cursor()	
+    '''returns data from any sql query'''
+    conn = pymssql.connect(host='server\sql2008', user='python', password='sql', database='northwind', as_dict='True')
+    cur = conn.cursor() 
 
-	cur.execute('%s' % sql)
+    cur.execute('%s' % sql)
 
-	return cur.fetchall()
+    return cur.fetchall()
 
 def getDoc(docid):
-	'''returns xml data from a given docid'''
-	xml = sqlData('select xml from xml where pkid = %s' % docid)
-	a = xml.pop()
-	return a['xml']
-	
+    '''returns xml data from a given docid'''
+    xml = sqlData('select xml from xml where pkid = %s' % docid)
+    a = xml.pop()
+    return a['xml']
+    
 def removeQuery(xml):
-	xml = re.sub('\[DOCUMENTQUERY.*?\]','',xml)
-	return xml
-	
+    xml = re.sub('\[DOCUMENTQUERY.*?\]','',xml)
+    return xml
+    
 def getDocQuery(docString):
-	docQuery = re.findall('(?<=\[DOCUMENTQUERY ").*?(?="\])','%s' % docString)
+    docQuery = re.findall('(?<=\[DOCUMENTQUERY ").*?(?="\])','%s' % docString)
 
-	a_dict = {}
-	for item in docQuery:
-		sqlQuery = re.sub('~ASKVALUE (.*?)~', lambda m: '{}'.format(askvalue[m.group(1)]) ,item)
-		result = sqlData('%s' % sqlQuery)
-		a_dict.update(result.pop())
-	return a_dict
+    a_dict = {}
+    for item in docQuery:
+        sqlQuery = re.sub('~ASKVALUE (.*?)~', lambda m: '{}'.format(askvalue[m.group(1)]) ,item)
+        result = sqlData('%s' % sqlQuery)
+        a_dict.update(result.pop())
+    return a_dict
 
 def formatGet(m):
-	contents = m.group(1)
-	print m.group(1)
-	try:
-		c2 = m.group(2)
-	except:
-		c2 = None
-	try:
-		c3 = m.group(3)
-	except:
-		c3 = None
-	print contents
-	if c2 != None:
-		print c2 
-	if c3 != None:
-		print c3
-	return '{}'.format(docResults[contents]) 
+    contents = m.group(1)
+    #print m.group(1)
+    print type(docResults[contents])
+    try:
+        c3 = m.group(3)
+    except:
+        c3 = None
+    if c3 != None:
+       pass #print c3
+
+    if type(docResults[contents]) == datetime:
+        pass    
+
+
+    return '{}'.format(docResults[contents]) 
 
 def fillGets(xml, dictionary):
-	xml = re.sub('\[get\((.*?)\)\]', formatGet ,xml)
-	xml = re.sub('\[MERGEFIELD (.*?)(\s"(.*)")?\]', formatGet ,xml)
-	print xml
+    xml = re.sub('\[get\((.*?)\)\]', formatGet ,xml)
+    xml = re.sub('\[MERGEFIELD (.*?)(\s"(.*)")?\]', formatGet ,xml)
+    #   \[[\s*]?MERGEFIELD[\s*]?(.*?)([\s*]?"(.*)?")?[\s*]?\]
+    return xml
+    
+def dateFormat(format):
+    dateFormatTable = [
+                       ('YYYY' , '%Y'  ),
+                       ('YY' , '%y' ),
+                       ('WW' , '%A' ),
+                       ('W' , '%a' ),
+                       ('MMMM' , '%B' ),
+                       ('MMM' , '%b' ),
+                       ('MM' , '%m' ),
+                       ('M' , '%m' ),
+                       ('HH:MM' , '%x' ),
+                       ('HH' , '%I' ),
+                       ('DD' , '%d' ),
+                       ('D', '%d' ) ]
+    
+    if format == None:
+        return '%x'
+    else:
+        for i in dateFormatTable:
+            format = str.replace(format, i[0], i[1])
+        return format
 
-	
-	
-xml = getDoc(askvalue['docid'])	
-docResults	= getDocQuery(xml)
+import datetime
+now = datetime.datetime.now()
+dateformat = dateFormat('MM-DD-YYYY WW, MMMM DD, YYYY')
+print now.strftime('%s' % dateformat)
+
+
+xml = getDoc(askvalue['docid']) 
+docResults  = getDocQuery(xml)
 xml = removeQuery(xml)
-fillGets(xml, docResults)
+xml = fillGets(xml, docResults)
+
+# print xml
+
+
+
+
+
 #print (results)
 #Documentquery RegEx  '(?<=\[DOCUMENTQUERY).*(?=\])'
 #Get Regex '(?<=\[get\().*?(?=\)\])'
 
 #Get Regex from J '\[get\((.*?)\)\]'
-#					replace $1
+#                   replace $1
 
 
-	
-	
+    
+    
 
 
 #x = getDocQuery(xml)
