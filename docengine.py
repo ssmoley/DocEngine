2aa97734e007407d28f6481e44f8ba658de74894
sean <sean>
2012-08-19 22:57:15 -0400

    PEP-8 formatted DocStrings, comments
    Created fillMergeLoop, has bugs

--- docengine.py
+++ docengine.py
@@ -2,148 +2,164 @@ import datetime
 import re
 import pymssql
 
-askvalue = {'docid': 7,
+askvalue = {'docid': 8,
             'ORDER_NUMBER': '10250'}
 listNumCount = []
-askvalue = {'docid':7,
-            'ORDER_NUMBER':'10250' }
 
 
-def sqlData(sql, dict = False):
+def sqlData(sql, dict=False):
     '''returns data from any sql query'''
-    conn = pymssql.connect(host='server\sql2008', user='python', password='sql', database='northwind', as_dict='True')
-    cur = conn.cursor() 
+    conn = pymssql.connect(host='server\sql2008', user='python',
+                           password='sql', database='northwind',
+                           as_dict='True')
+    cur = conn.cursor()
 
     cur.execute('%s' % sql)
 
     return cur.fetchall()
+
 
 def getDoc(docid):
     '''returns xml data from a given docid'''
     xml = sqlData('select xml from xml where pkid = %s' % docid)
     a = xml.pop()
     return a['xml']
-    
+
+
 def removeQuery(xml):
-    xml = re.sub('\[DOCUMENTQUERY.*?\]','',xml)
+    '''clears any queries from the document after retrieving the data'''
+    xml = re.sub('\[DOCUMENTQUERY.*?\]', '', xml)
     return xml
-    
+
+
 def getDocQuery(docString):
-    docQuery = re.findall('(?<=\[DOCUMENTQUERY ").*?(?="\])','%s' % docString)
+    '''finds any DOCUMENTQUERY Strings and returns a dictionary with SQL
+       column names and values'''
+    docQuery = re.findall('(?<=\[DOCUMENTQUERY ").*?(?="\])',
+                          '%s' % docString)
 
     a_dict = {}
     for item in docQuery:
-        sqlQuery = re.sub('~ASKVALUE (.*?)~', lambda m: '{}'.format(askvalue[m.group(1)]) ,item)
+        sqlQuery = re.sub('~ASKVALUE (.*?)~',
+                          lambda m: '{}'.format(askvalue[m.group(1)]),
+                          item)
         result = sqlData('%s' % sqlQuery)
         a_dict.update(result.pop())
     return a_dict
 
+
 def formatGet(m):
+    '''Determines what format to use for values based on variable type
+    '''
     contents = m.group(1)
     #print m.group(1)
-    print type(docResults[contents])
+    #print type(docResults[contents])
     try:
-        c3 = m.group(3)
+        c3 = m.group(2)
     except:
         c3 = None
     if c3 != None:
-        pass #print c3
+        pass  # print c3
+    try:
+        if type(docResults[contents]) == type(datetime.datetime.now()):
+            result = docResults[contents]
+            c3 = dateFormat(c3)
+            return result.strftime(c3)
+        else:
+            return '{}'.format(docResults[contents])
+    except:
+        return '***ERROR RETRIEVING %s***' % contents
 
-    if type(docResults[contents]) == type(datetime.datetime.now()):
-        result = docResults[contents]
-        c3 = dateFormat(c3)
-        return result.strftime(c3)
-    else:
-        return '{}'.format(docResults[contents]) 
 
 def fillGets(xml, dictionary):
-    xml = re.sub('\[get\((.*?)\)\]', formatGet ,xml)
-    xml = re.sub('\[MERGEFIELD (.*?)(\s"(.*)")?\]', formatGet ,xml)
+    '''Finds all [get(VALUE)] and [MERGEFIELD VALUES] in xml and
+       replaces them with the values found in the passed dictionary.
+    '''
+    xml = re.sub('\[get\((.*?)\)\]', formatGet, xml)
+    xml = re.sub('\[MERGEFIELD (.*?)(?:\s"(.*)")?\]', formatGet, xml)
     #   \[[\s*]?MERGEFIELD[\s*]?(.*?)([\s*]?"(.*)?")?[\s*]?\]
     return xml
-    
+
+
 def dateFormat(format):
+    ''' Used by formatGet, this converts Docmaster date formatting to
+        python date formatting.
+
+        DMS date formatting is commented below
+
+        Todo: AM/PM Modifiers
+    '''
     dateFormatTable = [
-                       ('YYYY' , '%Y' ),
-                       ('YY' , '%y' ),
-                       ('WW' , '%A' ),
-                       ('W' , '%a' ),
-                       ('MMMM' , '%B' ),
-                       ('MMM' , '%b' ),
-                       ('MM' , '%m' ),
-                       ('M' , '%m' ),
-                       ('HH:MM' , '%x' ),
-                       ('HH' , '%I' ),
-                       ('DD' , '%d' ),
-                       ('D', '%d' ) 
+                       ('YYYY', '%Y'),   # 2012 - 4 Digit Year
+                       ('YY', '%y'),     # 12   - 2 Digit Year
+                       ('WW', '%A'),     # Tuesday - Day of Week
+                       ('W', '%a'),      # Tue  - Short Day of Week
+                       ('MMMM', '%B'),   # August - Month
+                       ('MMM', '%b'),    # Aug  - Short Month
+                       ('MM', '%m'),     # 08   - Month Number
+                       ('M', '%m'),      # 08   - Month Number (Legacy)
+                       ('HH:MM', '%x'),  # 11:47 - Time
+                       ('HH', '%I'),     # 11   - Hour
+                       ('DD', '%d'),     # 04   - Day of Week
+                       ('D', '%d')       # 04   - Day of Week (Legacy)
                       ]
-    
+
     if format == None:
-        return '%x'
+        return '%m/%d/%Y'
     else:
         for i in dateFormatTable:
             format = str.replace(format, i[0], i[1])
         return format
 
-# import datetime
-# now = datetime.datetime.now()
-# dateformat = dateFormat('MM-DD-YY WW, MMMM DD, YYYY')
-# print now.strftime('%s' % dateformat)
+
+def formatListNum(m):
+    ''' Used by fillListNum to count each List and return an int value
+
+        To do: Roman Numeral Format Type, may need new function,
+               or new markup language.
+    '''
+    global listNumCount
+    listNumCount.append(m.group(1))
+    c = listNumCount.count(m.group(1))
+    return '%s' % c
+    pass
 
 
-xml = getDoc(askvalue['docid']) 
-docResults  = getDocQuery(xml)
+def fillListNum(xml):
+    ''' Finds all LISTNUM markup and replaces it with sequential numbers
+    '''
+    return re.sub('\[LISTNUM (.*?)(\s"(.*)")?\]', formatListNum, xml)
+
+
+def fillMergeLoop(xml):
+    global docResults
+    text = ''
+
+    # \[MERGELOOP "(.*)"](.*)\[ENDMERGE\]
+    regexMatch = re.search('\[MERGELOOP "(.*)"](.*)\[ENDMERGE\]', xml, re.DOTALL)
+    loopQuery = fillGets(regexMatch.group(1), docResults)
+    loopData = sqlData(loopQuery)
+    loopText = regexMatch.group(2)
+
+    for item in loopData:
+        print loopText
+        print item['OrderDate']
+        print fillGets(loopText, item)
+        text = text + fillGets(loopText, item)
+        pass
+    print text
+
+
+xml = getDoc(askvalue['docid'])
+docResults = getDocQuery(xml)
 xml = removeQuery(xml)
+fillMergeLoop(xml)
 xml = fillGets(xml, docResults)
-
-print xml
-
-
-
-#print (results)
-#Documentquery RegEx  '(?<=\[DOCUMENTQUERY).*(?=\])'
-#Get Regex '(?<=\[get\().*?(?=\)\])'
-
-#Get Regex from J '\[get\((.*?)\)\]'
-#                   replace $1
-
-
-    
-    
-
-
-#x = getDocQuery(xml)
-#print x
+xml = fillListNum(xml)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print xml
 
 
 
