1dba840452049e526688eef1be69bb35eff9563d
sean <sean>
2012-08-30 23:04:20 -0400

    inital  app for opening all DMS templates, logging which have been completed.

---
+++ editdocs.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import glob
+
+files = glob.glob(r'c:\\users\\sean\\desktop\\python\\*.txt')
+complete = []
+s = ''
+i = 0
+
+f = open('complete.txt', 'r+')
+for line in f:
+    complete.append(line.strip())
+    print line.strip()
+f.close()
+
+for item in files:
+    if complete.count(item) == 0:
+        subprocess.call('notepad.exe %s' % item)
+        print 'count , Run Notepad %s' % item
+        complete.append('%s\n' % item)
+        i = +i
+    s = raw_input('Keep Going?')
+    if s != '':
+        break
+    else:
+        continue
+
+
+
+f = open('complete.txt', 'w+')
+for item in complete:
+    f.write('%s\n' % item)
+
+f.close()
